ba423de50474210ee65fcd237d2dd5a9d0cd856b
Engr-Henry <st.henry2013@gmail.com>
2023-05-02 13:17:28 -0700

    ntask

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,40 +1,58 @@
 #!/usr/bin/env python3
-""" Authentication Module."""
+"""
+Definition of class Auth
+"""
 from flask import request
-from typing import List, TypeVar
+from typing import (
+    List,
+    TypeVar
+)
 
 
 class Auth:
-    """ Manage API Authentication """
+    """
+    Manages the API authentication
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Checks if a path requires authentication. """
+        """
+        Determines whether a given path requires authentication or not
+        Args:
+            - path(str): Url path to be checked
+            - excluded_paths(List of str): List of paths that do not require
+              authentication
+        Return:
+            - True if path is not in excluded_paths, else False
+        """
         if path is None:
             return True
-        if not excluded_paths:
+        elif excluded_paths is None or excluded_paths == []:
             return True
-        if path in excluded_paths:
+        elif path in excluded_paths:
             return False
         else:
-            for paths in excluded_paths:
-                if paths.startswith(path):
+            for i in excluded_paths:
+                if i.startswith(path):
                     return False
-                if path.startswith(paths):
+                if path.startswith(i):
                     return False
-                if paths.endswith('*'):
-                    if path.startswith(paths[:-1]):
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
                         return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Checks request header for Authorization. """
+        """
+        Returns the authorization header from a request object
+        """
         if request is None:
             return None
         header = request.headers.get('Authorization')
         if header is None:
             return None
-        else:
-            return header
+        return header
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ checks current user """
+        """
+        Returns a User instance from information from a request object
+        """
         return None
