e0e62f093993c41c579f40abc9a93dd3e537c3e9
Engr-Henry <st.henry2013@gmail.com>
2023-05-03 13:12:15 -0700

    ntasks

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-""" UserSession module
-"""
-from models.base import Base
+""" User session Module. """
+from .base import Base
 
 
 class UserSession(Base):
-    """
-    UserSession class
-    """
-
+    """ User session class """
     def __init__(self, *args: list, **kwargs: dict):
-        """
-        Initialize a UserSession instance
-        """
+        """ Initialize user session instance. """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
